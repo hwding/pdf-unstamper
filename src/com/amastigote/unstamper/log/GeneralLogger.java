a4c29f38fd4f331539004dd08b01930df750bc6a
hwding <m@amastigote.com>
2018-12-25 21:54:51 +0800

    🔨 burp version to 0.2.1

--- src/com/amastigote/unstamper/log/GeneralLogger.java
+++ src/com/amastigote/unstamper/log/GeneralLogger.java
@@ -12,7 +12,7 @@ import com.sun.istack.internal.NotNull;
 public class GeneralLogger {
     public static class Help {
         private static final String usage =
-                "\nPDF-UnStamper ver. 0.1.5 by hwding@GitHub\n" +
+                "\nPDF-UnStamper ver. 0.2.1 by hwding@GitHub\n" +
                         "\nUsage:" +
                         "\n   [OPTION] -i [INPUT PDF] -k [KEYWORDS...] (-o [OUTPUT PDF])" +
                         "\n   [OPTION] -I [INPUT DIR] -k [KEYWORDS...] (-O [OUTPUT DIR])\n" +
