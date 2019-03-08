ed26180576b99c6ef3984af43720999406dd793c
hwding <m@amastigote.com>
2019-03-08 23:34:38 +0800

    🔨 bump version to 0.2.5

--- src/com/amastigote/unstamper/Main.java
+++ src/com/amastigote/unstamper/Main.java
@@ -17,7 +17,7 @@ import org.apache.commons.cli.ParseException;
 
 public class Main {
 
-    public static final String _version = "0.2.4";
+    public static final String _version = "0.2.5";
 
     static {
         /* Disable Logging in Apache PDFBox */
